eaaade4abceb1da131d54d89b442721ac8bafbb1
nitiwat <nitiwatcmu@gmail.com>
2023-10-04 21:52:27 +0700

    CRUD

--- client/src/formData/FormUpdate.jsx
+++ client/src/formData/FormUpdate.jsx
@@ -1,33 +1,68 @@
-import { useState } from "react"
-import { NavLink } from "react-router-dom"
+import { useState, useEffect, useRef } from "react"
+import { NavLink, useParams } from "react-router-dom"
 import { DataBrand } from './DataBrand/DataBrand'
 import { DataYear } from "./DataBrand/DataYear"
 import Axios from 'axios'
 const FormUpdatedata = ({ setBrand, setName, setYear, setCost }) => {
 
+    const { id } = useParams();
+    const [readData, setReadData] = useState([])
+
+    const GetData = () => {
+        Axios.get("http://localhost:3002/read/" + id)
+            .then((res) => {
+                setReadData((res.data))
+            })
+    }
+
+    useEffect(() => {
+        GetData();
+        setBrand(selectRef.current.value)
+        setName(selectRefName.current.value)
+        setYear(selectRefYear.current.value)
+
+
+
+    })
+
+
+
+
+    const selectRef = useRef(null)
+    const selectRefName = useRef(null)
+    const selectRefYear = useRef(null)
+    const selectCost = useRef(0)
+
     const [Gen, setGen] = useState([])
-
-
-
-
 
     const hangleChangeValue = (e) => {
         setBrand(e.target.value)
-        setGen(DataBrand.find(brn => brn.name === e.target.value).Gen)
+
     }
+
+    useEffect(() => {
+        setGen(DataBrand.find(brn => brn.name === selectRef.current.value).Gen)
+    })
 
     return (
         <>
             <div className="mb-3">
                 <label htmlFor="brand" className='form-label text-white fw-bold' >Change Brand</label>
-                <select id="cars" className='form-select' aria-label="Default select  " required 
+                <select id="cars" className='form-select' aria-label="Default select  " required ref={selectRef}
                     onClick={hangleChangeValue}  >
-                   <option value="" key={1}  disabled defaultValue='BRAND' selected  label="--Brand--"></option>
+
+                    {readData.map((val, i) => {
+                        return (
+                            
+                                <option key={i} selected value={val.brand} >{val.brand}</option>
+                            
+                        )
+                    })}
                     {DataBrand.map((val, i) => {
                         return (
-                            <>
+                            
                                 <option key={i} value={val.name} >{val.name}</option>
-                            </>
+                            
                         )
                     })}
 
@@ -36,12 +71,13 @@ const FormUpdatedata = ({ setBrand, setName, setYear, setCost }) => {
 
             <div className="mb-3">
                 <label htmlFor="name" className='form-label text-white fw-bold'>Change Name</label>
-                <select id="cars" className='form-select' aria-label="Default select  " required
+                <select id="cars" className='form-select' aria-label="Default select  " required ref={selectRefName}
                     onClick={(e) => { setName(e.target.value) }}>
-                          <option value=""  key={1}  selected   disabled label="--Name--"></option>
+
+
                     {Gen.map((val, i) => {
                         return (
-                            <option key={i} value={val.name}  >{val.name}</option>
+                            <option key={i} value={val.name} selected >{val.name}</option>
                         )
                     })}
                 </select>
@@ -49,9 +85,16 @@ const FormUpdatedata = ({ setBrand, setName, setYear, setCost }) => {
 
             <div className="mb-3">
                 <label htmlFor="year" className='form-label text-white fw-bold'>Change Year</label>
-                <select id="cars" className='form-select' aria-label="Default select " required
+                <select id="cars" className='form-select' aria-label="Default select " required ref={selectRefYear}
                     onClick={(e) => { setYear(e.target.value) }}>
-                    <option value="" selected key={1}  disabled  label="--Year--"></option>
+
+                    {readData.map((val, i) => {
+                        return (
+                          
+                                <option key={i} selected value={val.year} >{val.year}</option>
+                           
+                        )
+                    })}
                     {DataYear.map((val, i) => {
                         return (
                             <option key={i} value={val.year} >{val.year}</option>
@@ -61,8 +104,11 @@ const FormUpdatedata = ({ setBrand, setName, setYear, setCost }) => {
             </div>
             <div className='mb-3'>
                 <label htmlFor="cost" className='form-label text-white fw-bold'>Change Cost</label>
-                <input type="number" className='form-control' required placeholder='Enter Cost'
+
+                <input type="number" className='form-control' required
                     onChange={(e) => { setCost(e.target.value) }} />
+
+
             </div>
 
 
